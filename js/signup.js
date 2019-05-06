38e26643d6c70fd2e5b4f8fa10d7eef30dbdba2a
Emmanuel Escobedo <emanuel2867@gmail.com>
2019-05-06 18:24:05 -0500

    Front-1

---
+++ js/signup.js
@@ -0,0 +1,39 @@
+
+$('#signup_button').on('click', function(){
+  
+  const email = $("#email").val()
+  const password = $("#password").val()
+  const name = $("#name").val()
+  const age = $("#age").val()
+
+  json_to_send = {
+    "password" : password,
+    "email": email,
+    "name": name,
+    "age": age
+  };
+  console.log(json_to_send)
+
+
+  json_to_send = JSON.stringify(json_to_send);
+
+  $.ajax({
+    //url: 'http://localhost:3000/users',
+    url: 'https://examenfinalyehrson.herokuapp.com/users',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    method: 'POST',
+    dataType: 'json',
+    data: json_to_send,
+    success: function(data){
+      alert("Usuario creado con exito");
+      console.log('success: '+ data);
+      window.location = './index.html'
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+
+});
